97262c32078d487a721aab08a3f830800aff82af
Ariana <73635827+arianadaris@users.noreply.github.com>
2021-11-07 01:37:46 -0700

    Update Header.js

--- Components/Header.js
+++ Components/Header.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 import {Link} from 'react-router-dom';
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
 function Header()
 {
     return (
         <Container>
             <Wrap>
-                <ItemText><Link to="/" style={{ textDecoration: 'none' }}>Ariana Daris</Link></ItemText>
+                <ItemText><StyledLink to="/" style={{ textDecoration: 'none' }}>Ariana Daris</StyledLink></ItemText>
                 <Nav>
                     <List>
-                        <ListItem><Link to="/about" style={{ textDecoration: 'none' }}>About</Link></ListItem>
-                        <ListItem><Link to="/work" style={{ textDecoration: 'none' }}>Work</Link></ListItem>
-                        <ListItem><Link to="/contact" style={{ textDecoration: 'none' }}>Contact</Link></ListItem>
+                        <ListItem><StyledNavLink to="/about" activeClassName="active" style={{ textDecoration: 'none' }}>About</StyledNavLink></ListItem>
+                        <ListItem><StyledNavLink to="/work" activeClassName="active" style={{ textDecoration: 'none' }}>Work</StyledNavLink></ListItem>
+                        <ListItem><StyledNavLink to="/contact" activeClassName="active" style={{ textDecoration: 'none' }}>Contact</StyledNavLink></ListItem>
                     </List>
                 </Nav>
             </Wrap>
@@ -22,12 +23,26 @@ function Header()
 
 export default Header;
 
+const appearKey = keyframes`
+    0%
+    {
+        opacity: 0;
+    }
+    100%
+    {
+        opacity: 100%;
+    }
+`
+
 const Container = styled.div`
     position: fixed;
     width: 100%;
     background-color: white;
     box-shadow: 0 3px 3px rgba(0,0,0,0.1);
     z-index: 1500;
+    opacity: 0;
+    animation: 1s ease-out 0s 1 ${appearKey};
+    animation-fill-mode: forwards;
 `
 
 const Wrap = styled.div`
@@ -59,5 +74,23 @@ const List = styled.ul`
 const ListItem = styled.li`
     font-size: 24px;
     margin-right: 50px;
-    cursor: pointer;
+    border-bottom: 1.5px solid white;
+`
+
+const StyledNavLink = styled(NavLink)`
+    &:hover
+    {
+        color: #80C3C3;
+        border-bottom: 1.5px solid #7DB6A6;
+    }
+    transition: 0.2s ease all;
+}
+`
+
+const StyledLink = styled(Link)`
+    &:hover
+    {
+        color: #80C3C3;
+    }
+    transition: 0.2s ease all;
 `
