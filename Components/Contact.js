c27458e38da6a876ac243db7ef45553e5ab76813
Ariana <73635827+arianadaris@users.noreply.github.com>
2021-11-07 01:37:56 -0700

    Update Contact.js

--- Components/Contact.js
+++ Components/Contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Footer from './Footer';
 import Background from './Background';
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
 function Contact()
 {
@@ -13,32 +13,50 @@ function Contact()
                 <Wrap>
                     <TextH1>CONTACT ME</TextH1>
                     <TextH3>Let's Create Something Together!</TextH3>
+                    <ButtonGroup>
+                        <a href="mailto:arianarajewski@gmail.com" style={{ textDecoration: 'none' }}>
+                            <EmailBtn>
+                                <img src="https://img.icons8.com/material-rounded/35/FFFFFF/mail.png"/>
+                                <ImgH1 className="mail">E-mail me!</ImgH1>
+                            </EmailBtn>
+                        </a>
+                        <a href="https://www.instagram.com/arianadaris/" target="_blank" rel="noreferrer">
+                            <InstaBtn>
+                                <img src="https://img.icons8.com/material-outlined/35/FFFFFF/instagram-new--v1.png" alt="Instagram"/>
+                                <ImgH1 className="insta">My Instagram!</ImgH1>
+                            </InstaBtn>
+                        </a>
+                        <a href="https://github.com/arianadaris" target="_blank" rel="noreferrer">
+                            <GitHubBtn>
+                                <img src="https://img.icons8.com/material-outlined/35/FFFFFF/github.png" alt="GitHub"/>
+                                <ImgH1 className="github">My GitHub!</ImgH1>
+                            </GitHubBtn>
+                        </a>
+                        <a href="https://www.linkedin.com/in/ariana-rajewski/" target="_blank" rel="noreferrer">
+                            <LinkedBtn>
+                                <img src="https://img.icons8.com/ios-glyphs/35/FFFFFF/linkedin.png" alt="LinkedIn"/>
+                                <ImgH1 className="linked">My LinkedIn!</ImgH1>
+                            </LinkedBtn>
+                        </a>
+                    </ButtonGroup>
                 </Wrap>
             </Section>
-            <Form>
-                <FormItem>
-                    <Label>Name</Label>
-                    <Input />
-                </FormItem>
-                <FormItem>
-                    <Label>Email</Label>
-                    <Input />
-                </FormItem>
-                <FormItem>
-                    <Label>Subject</Label>
-                    <Input />
-                </FormItem>
-                <FormItem>
-                    <Label>Message</Label>
-                    <TextArea></TextArea>
-                </FormItem>
-            </Form>
-            <Footer />
         </Container>
     );
 }
 
 export default Contact;
+
+const appearKey = keyframes`
+    0%
+    {
+        opacity: 0;
+    }
+    100%
+    {
+        opacity: 100%;
+    }
+`
 
 const Container = styled.div`
     width: 100%;
@@ -46,19 +64,24 @@ const Container = styled.div`
     margin-top: 15vh;
     position: absolute;
     overflow: hidden;
+    opacity: 0;
+    animation: 1s ease-out 0s 1 ${appearKey};
+    animation-fill-mode: forwards;
 `
 
 const Section = styled.div`
     height: auto;
+    margin-top: 5%;
+    display: flex;
+    justify-content: center;
 `
 
 const Wrap = styled.div`
     z-index: 100;
-    padding: 10px 0 0 165px;
 `
 
 const TextH1 = styled.h1`
-    font-size: 32px;
+    font-size: 26px;
     font-style: italic;
     color: #61B29C;
 `
@@ -69,38 +92,75 @@ const TextH3 = styled.h3`
     line-height: 80px;
 `
 
-const FormItem = styled.div`
+const ButtonGroup = styled.div`
+    z-index: 20;
+    padding: 50px;
     display: flex;
     justify-content: center;
-    padding: 20px;
 `
 
-const Form = styled.form`
-    padding: 20px;
+const ImgH1 = styled.h1`
+    color: #127173;
+    font-size: 15px;
+    text-align: center;
+    margin-left: -50%;
+    opacity: 0;
+    transition: 0.2s all;
 `
 
-const Label = styled.label`
-    font-size: 20px;
-    margin-right: 20px;
-`
+const Button = styled.button`
+    img
+    {
+        background-color: #7DB6A6;
+        box-shadow: 4px 4px 6px rgba(0, 0, 0, 0.2);
+        margin-right: 50px;
+        padding: 5px;
+        border-radius: 20%;
+        transition: 0.2s ease;
+    }
 
-const Input = styled.input`
+    &:hover img
+    {
+        box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.35);
+        background-color: #80C3C3;
+        margin-top: -10px;
+    }
+
     border: none;
-    padding: 10px;
-    background-color: rgba(0,0,0,0.1);
-    box-shadow: 1px 3px 6px rgba(0,0,0,0.3);
-    border-radius: 20px;
-
+    background-color: white;
+    cursor: pointer;
+    transition: 0.2s ease;
 `
 
-const TextArea = styled.textarea`
-    border: none;
-    padding: 10px;
-    background-color: rgba(0,0,0,0.1);
-    box-shadow: 1px 3px 6px rgba(0,0,0,0.3);
-    border-radius: 20px;
+const EmailBtn = styled(Button)`
+    &:hover .mail
+    {
+        margin-top: 15px;
+        opacity: 100%;
+    }
 `
-
-const SubmitBtn = styled.input`
-
+ 
+const InstaBtn = styled(Button)`
+    &:hover .insta
+    {
+        margin-top: 15px;
+        opacity: 100%;
+    }
 `
+ 
+const GitHubBtn = styled(Button)`
+    &:hover .github
+    {
+        margin-top: 15px;
+        opacity: 100%;
+    }
+`
+ 
+const LinkedBtn = styled(Button)`
+    &:hover .linked
+    {
+        margin-top: 15px;
+        opacity: 100%;
+    }
+`
+ 
