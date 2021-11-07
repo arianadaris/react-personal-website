f368688121207f1b314b83384a709704b49efe41
Ariana <73635827+arianadaris@users.noreply.github.com>
2021-11-07 01:37:24 -0700

    Update Background.js

--- Components/Background.js
+++ Components/Background.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 import {ReactComponent as Waves1} from '../images/Waves1.svg';
 import {ReactComponent as Waves2} from '../images/Waves2.svg';
 
@@ -8,10 +8,10 @@ function Background()
     return (
         <Bg>
             <Waves1Wrap>
-                <Waves1 />
+                <MoveWaves1 />
             </Waves1Wrap>
             <Waves2Wrap>
-                <Waves2 />
+                <MoveWaves2 />
             </Waves2Wrap>
         </Bg>
     )
@@ -38,4 +38,31 @@ const Waves2Wrap = styled.div`
     margin-top: -1025px;
     margin-left: 550px;
     z-index: -1;
+`
+
+const waveMotion = keyframes`
+    0%
+    {
+        transform: scaleX(1);
+    }
+    50%
+    {
+        transform: scaleX(1.3);
+    }
+    100%
+    {
+        transform: scaleX(1);
+    }
+`
+
+const MoveWaves1 = styled(Waves1)`
+    animation-name: ${waveMotion};
+    animation-duration: 8s;
+    animation-iteration-count: infinite;
+`
+
+const MoveWaves2 = styled(Waves2)`
+    animation-name: ${waveMotion};
+    animation-duration: 8s;
+    animation-iteration-count: infinite;
 `
